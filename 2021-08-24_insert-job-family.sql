2c5510ccaf71ac4e2fe7c5912e01874ff9be4dda
Callum Kindred <callum-luis.kindred@kainos.com>
2021-08-24 11:57:08 +0100

    add sql file to populate capabilities & job families

---
+++ 2021-08-24_insert-job-family.sql
@@ -0,0 +1,63 @@
+INSERT INTO Capability (capabilityName) VALUES ("Artificial Intelligence");
+INSERT INTO Capability (capabilityName) VALUES ("Business Development & Marketing");
+INSERT INTO Capability (capabilityName) VALUES ("Product");
+INSERT INTO Capability (capabilityName) VALUES ("Cyber Security");
+INSERT INTO Capability (capabilityName) VALUES ("Data");
+INSERT INTO Capability (capabilityName) VALUES ("Engineering");
+INSERT INTO Capability (capabilityName) VALUES ("Experience Design");
+INSERT INTO Capability (capabilityName) VALUES ("Operations");
+INSERT INTO Capability (capabilityName) VALUES ("Platforms");
+INSERT INTO Capability (capabilityName) VALUES ("People");
+
+INSERT INTO JobFamily VALUES ("Artificial Intelligence (AI) Strategy and Planning",
+"Artificial Intelligence");
+INSERT INTO JobFamily VALUES ("Artificial Intelligence (AI) Engineering",
+"Artificial Intelligence");
+INSERT INTO JobFamily VALUES ("Data Science", "Artificial Intelligence");
+
+INSERT INTO JobFamily VALUES ("Strategy and Planning", "Business Development & Marketing");
+INSERT INTO JobFamily VALUES ("Business Development", "Business Development & Marketing");
+INSERT INTO JobFamily VALUES ("Client Management", "Business Development & Marketing");
+INSERT INTO JobFamily VALUES ("Partners", "Business Development & Marketing");
+INSERT INTO JobFamily VALUES ("Bid Production", "Business Development & Marketing");
+INSERT INTO JobFamily VALUES ("Inside Sales", "Business Development & Marketing");
+
+INSERT INTO JobFamily VALUES ("Product Consultancy", "Product");
+INSERT INTO JobFamily VALUES ("Digital Advisory Consultancy", "Product");
+INSERT INTO JobFamily VALUES ("Specialist Consultancy", "Product");
+
+INSERT INTO JobFamily VALUES ("Security Strategy and Planning", "Cyber Security");
+INSERT INTO JobFamily VALUES ("Security Engineering", "Cyber Security");
+INSERT INTO JobFamily VALUES ("Corporate Security", "Cyber Security");
+
+INSERT INTO JobFamily VALUES ("Data Strategy and Planning", "Data");
+INSERT INTO JobFamily VALUES ("Data Engineering", "Data");
+INSERT INTO JobFamily VALUES ("Analysis & Data Architecture", "Data");
+
+INSERT INTO JobFamily VALUES ("Engineering Strategy and Planning", "Engineering");
+INSERT INTO JobFamily VALUES ("Engineering", "Engineering");
+INSERT INTO JobFamily VALUES ("Architecture", "Engineering");
+INSERT INTO JobFamily VALUES ("Testing and Quality Assurance", "Engineering");
+INSERT INTO JobFamily VALUES ("Product Specialist", "Engineering");
+
+INSERT INTO JobFamily VALUES ("Insights", "Experience Design");
+INSERT INTO JobFamily VALUES ("Content Design", "Experience Design");
+INSERT INTO JobFamily VALUES ("UX / Visual Design", "Experience Design");
+INSERT INTO JobFamily VALUES ("Service Design", "Experience Design");
+
+INSERT INTO JobFamily VALUES ("Operations Strategy and Planning", "Operations");
+INSERT INTO JobFamily VALUES ("Operational Management", "Operations");
+INSERT INTO JobFamily VALUES ("Staff Management", "Operations");
+INSERT INTO JobFamily VALUES ("Programme Management Office (PMO)", "Operations");
+
+INSERT INTO JobFamily VALUES ("Platform Strategy and Planning", "Platforms");
+INSERT INTO JobFamily VALUES ("Platform Engineering", "Platforms");
+INSERT INTO JobFamily VALUES ("Platform Architecture", "Platforms");
+INSERT INTO JobFamily VALUES ("Platform Specialists", "Platforms");
+INSERT INTO JobFamily VALUES ("Cloud Migration", "Platforms");
+INSERT INTO JobFamily VALUES ("Systems Support", "Platforms");
+
+INSERT INTO JobFamily VALUES ("People Strategy and Planning", "People");
+INSERT INTO JobFamily VALUES ("People Support", "Platforms");
+INSERT INTO JobFamily VALUES ("Organisational Development & Learning (OD&L)", "Platforms");
+INSERT INTO JobFamily VALUES ("Talent Acquisition", "Platforms");
--- src/main/java/com/kainos/ea/backend/models/Test.java
+++ src/main/java/com/kainos/ea/backend/models/Test.java
@@ -2,8 +2,10 @@ package com.kainos.ea.backend.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "test")
 public class Test {
     @Id
     private String test;
