632931b667bb6266fc9d6bbfe0359427a1c771f6
Theo Hodges <theo.hodges@kainos.com>
2021-08-24 15:23:32 +0100

    GET /band-training now takes a parameter of band name and returns the training that belongs to this band

--- src/main/java/com/kainos/ea/backend/controllers/BandTrainingController.java
+++ src/main/java/com/kainos/ea/backend/controllers/BandTrainingController.java
@@ -1,13 +1,12 @@
 package com.kainos.ea.backend.controllers;
 
-
-import com.kainos.ea.backend.models.BandTraining;
 import com.kainos.ea.backend.models.Training;
 import com.kainos.ea.backend.service.BandTrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -18,7 +17,7 @@ public class BandTrainingController {
     BandTrainingService bandTrainingService;
 
     @GetMapping("/band-training")
-    public @ResponseBody Iterable<BandTraining> getAllTraining() {
-        return bandTrainingService.getAllTraining();
+    public @ResponseBody Iterable<Training> getTrainingByBand(@RequestParam String bandName) {
+        return bandTrainingService.getTrainingByBand(bandName);
     }
 }
--- src/main/java/com/kainos/ea/backend/service/BandTrainingService.java
+++ src/main/java/com/kainos/ea/backend/service/BandTrainingService.java
@@ -1,9 +1,15 @@
 package com.kainos.ea.backend.service;
 
 import com.kainos.ea.backend.models.BandTraining;
+import com.kainos.ea.backend.models.Training;
 import com.kainos.ea.backend.repositories.BandTrainingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.StreamSupport.stream;
 
 @Service
 public class BandTrainingService {
@@ -11,7 +17,11 @@ public class BandTrainingService {
     @Autowired
     BandTrainingRepository bandTrainingRepository;
 
-    public Iterable<BandTraining> getAllTraining() {
-        return bandTrainingRepository.findAll();
+    public Iterable<Training> getTrainingByBand(String bandName) {
+        Iterable<BandTraining> bandTrainings = bandTrainingRepository.findByBandBandName(bandName);
+        Iterator<Training> trainingIterator = stream(bandTrainings.spliterator(), true)
+                .map(BandTraining::getTraining)
+                .iterator();
+        return () -> trainingIterator;
     }
 }
