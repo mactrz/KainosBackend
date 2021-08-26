e9340fc244654b7eb4d6ce90b6c61e2d14bb3e27
Sebastian Czajkowski <sebastian.czajkowski@kainos.com>
2021-08-26 14:47:38 +0200

    Change service name

--- src/main/java/com/kainos/ea/backend/controllers/JobRoleController.java
+++ src/main/java/com/kainos/ea/backend/controllers/JobRoleController.java
@@ -1,9 +1,8 @@
 package com.kainos.ea.backend.controllers;
 
 import com.kainos.ea.backend.models.JobRole;
-import com.kainos.ea.backend.services.JobRoleService;
+import com.kainos.ea.backend.services.JobRolesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +11,12 @@ import org.springframework.web.bind.annotation.*;
 public class JobRoleController {
 
     @Autowired
-    JobRoleService jobRoleService;
+    JobRolesService jobRolesService;
 
     @GetMapping(path = "/band-level")
     public @ResponseBody
     Iterable<JobRole> getAllJobRolesSortedByBandName() {
-        return jobRoleService.getAllJobRolesSortByBandName();
+        return jobRolesService.getAllJobRolesSortByBandName();
     }
 
 }
--- src/main/java/com/kainos/ea/backend/services/JobRolesService.java
+++ src/main/java/com/kainos/ea/backend/services/JobRolesService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class JobRoleService {
+public class JobRolesService {
 
     @Autowired
     JobRolesRepository jobRolesRepository;
