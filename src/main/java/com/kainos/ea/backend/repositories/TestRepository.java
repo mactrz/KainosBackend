5feb0a4a1af5529f3ea984da0ab97caaa152ff12
Michal Tangri <michal.tangri@kainos.com>
2021-08-26 10:18:17 +0200

    Remove test classes made for POC

--- src/main/java/com/kainos/ea/backend/repositories/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kainos.ea.backend.repositories;
-
-import com.kainos.ea.backend.models.Test;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface TestRepository extends CrudRepository<Test, String> {
-
-}
