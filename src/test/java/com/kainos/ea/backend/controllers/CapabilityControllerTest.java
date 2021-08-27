cc817445d31c200c1ae9624a65812e91ec4f69f7
Sebastian Czajkowski <sebastian.czajkowski@kainos.com>
2021-08-27 13:35:57 +0200

    Remove unnecessary imports

--- src/test/java/com/kainos/ea/backend/controllers/CapabilityControllerTest.java
+++ src/test/java/com/kainos/ea/backend/controllers/CapabilityControllerTest.java
@@ -2,25 +2,11 @@ package com.kainos.ea.backend.controllers;
 
 import com.kainos.ea.backend.BackendApplicationTests;
 import com.kainos.ea.backend.models.Capability;
-import com.kainos.ea.backend.models.JobRole;
 import com.kainos.ea.backend.services.CapabilityService;
-import com.kainos.ea.backend.services.JobRolesService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
--- src/test/java/com/kainos/ea/backend/controllers/JobRoleControllerTest.java
+++ src/test/java/com/kainos/ea/backend/controllers/JobRoleControllerTest.java
@@ -2,24 +2,12 @@ package com.kainos.ea.backend.controllers;
 
 import com.kainos.ea.backend.models.JobRole;
 import com.kainos.ea.backend.services.JobRolesService;
-import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
