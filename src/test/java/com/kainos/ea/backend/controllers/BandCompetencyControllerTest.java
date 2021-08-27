b7d5a34496bf96b6660c39b3dfdb5e7aa7c65f9c
Maciej Trząsalski <maciej.trzasalski@kainos.com>
2021-08-27 12:22:21 +0200

    begin test fix

--- src/test/java/com/kainos/ea/backend/controllers/BandCompetencyControllerTest.java
+++ src/test/java/com/kainos/ea/backend/controllers/BandCompetencyControllerTest.java
@@ -1,77 +1,47 @@
 package com.kainos.ea.backend.controllers;
 
+
 import com.kainos.ea.backend.services.BandCompetencyService;
-import com.kainos.ea.backend.services.BandCompetencyService.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-class BandCompetencyControllerTest {
 
 
-    @LocalServerPort
-    private final int port = 8080;
+@ExtendWith(MockitoExtension.class)
+class JobRoleControllerTest {
 
-    TestRestTemplate restTemplate = new TestRestTemplate();
-    HttpHeaders headers = new HttpHeaders();
+    @Mock
+    private BandCompetencyService bandCompetencyService;
 
     @Test
-    public void getCompetenciesByBandHappyPathId() throws Exception {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/competency/Associate"), HttpMethod.GET, entity, String.class);
-        String expected = "\"id\":9";
+    public void when_querying_getCompetenciesByBand_expect_ServiceCalledPassback(){
+        Iterable<JobRole> jobRoles = Collections.emptyList();
+        Mockito.when(jobRolesService.getAllJobRolesSortByBandName()).thenReturn(jobRoles);
+        JobRoleController jobRoleController = new JobRoleController(jobRolesService);
 
-        System.out.println(response.getBody());
-        JSONAssert.assertEquals(expected, response.getBody().substring(2,8), false);
+        Iterable<JobRole> results = jobRoleController.getAllJobRolesSortedByBandName();
+        // check if the service has been called
+        Mockito.verify(jobRolesService).getAllJobRolesSortByBandName();
+
+        assertEquals(jobRoles, results);
     }
 
     @Test
-    public void getCompetenciesByBandNoBandNotFound() throws Exception {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/competency/"), HttpMethod.GET, entity, String.class);
-        int expected = 404;
+    public void when_QueryingAllJobRolesSortedByCapability_expect_ServiceCalledPassback(){
+        List<JobRole> jobRoles = List.of(new JobRole());
+        Mockito.when(jobRolesService.getAllJobRolesSortedByCapability()).thenReturn(jobRoles);
 
-        System.out.println(response.getBody());
-        assertEquals(expected, response.getStatusCodeValue());
+        JobRoleController jobRoleController = new JobRoleController(jobRolesService);
+
+        Iterable<JobRole> results = jobRoleController.getAllJobRolesSortedByCapability();
+        // check if the service has been called
+        Mockito.verify(jobRolesService).getAllJobRolesSortedByCapability();
+
+        assertEquals(jobRoles, results);
     }
-
-    @Test
-    public void getCompetenciesByBandBadBandNameEmptyList() throws Exception {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/competency/asdasd"), HttpMethod.GET, entity, String.class);
-        String expected = "[]";
-
-        System.out.println(response.getBody());
-        JSONAssert.assertEquals(expected, response.getBody(), false);
-    }
-
-    @Test
-    public void getCompetenciesByBandBadBandHappyPathNotNull() throws Exception {
-        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
-        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/competency/Associate"), HttpMethod.GET, entity, String.class);
-        String expected = "[]";
-
-        System.out.println(response.getBody());
-        assertFalse(response.getBody().isBlank());
-    }
-
-    private String createURLWithPort(String uri) {
-
-        return "http://localhost:" + port + uri;
-    }
+}
 }
