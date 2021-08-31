b6f4a04c9567b4badbb374a659df64150d32ed1f
Sebastian Czajkowski <sebastian.czajkowski@kainos.com>
2021-08-31 10:35:36 +0200

    Change default sharepoint url and response message

--- src/main/java/com/kainos/ea/backend/models/JobRole.java
+++ src/main/java/com/kainos/ea/backend/models/JobRole.java
@@ -22,7 +22,7 @@ public class JobRole {
     private String specification;
 
     @Column(name = "sharepointURL")
-    private String sharepointUrl = "URL not provided yet";
+    private String sharepointUrl = "https://kainossoftwareltd.sharepoint.com/people/SitePages/Career-Lattice.aspx";
 
     @Column(name = "responsibilites")
     private String responsibilites = "Responsibilities not provided yet";
