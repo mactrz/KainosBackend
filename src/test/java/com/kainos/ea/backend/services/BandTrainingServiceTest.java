38b8e68bda3adbd9973ee2c85bde9f0ea8bf7b77
TheoHodges <65924703+TheoHodges@users.noreply.github.com>
2021-09-06 11:47:06 +0100

    Change endpoint to require a recommended boolean

--- src/test/java/com/kainos/ea/backend/services/BandTrainingServiceTest.java
+++ src/test/java/com/kainos/ea/backend/services/BandTrainingServiceTest.java
@@ -4,6 +4,7 @@ import com.kainos.ea.backend.models.Band;
 import com.kainos.ea.backend.models.BandTraining;
 import com.kainos.ea.backend.models.Training;
 import com.kainos.ea.backend.repositories.BandTrainingRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -20,17 +21,46 @@ public class BandTrainingServiceTest {
     @Mock
     private BandTrainingRepository bandTrainingRepository;
 
-    @Test
-    public void when_QueryingTrainingByBand_expect_RepositoryCalledPassback() {
+    private BandTrainingService bandTrainingService;
+
+    private List<Training> trainings;
+    private List<BandTraining> bandTrainings;
+
+    @BeforeEach
+    public void setUp() {
+        bandTrainingService = new BandTrainingService(bandTrainingRepository);
         Band band = new Band();
         Training training = new Training();
-        List<Training> trainings = List.of(training);
-        List<BandTraining> bandTrainings = List.of(new BandTraining((short) 0, band, training, true));
+        trainings = List.of(training);
+        bandTrainings = List.of(new BandTraining((short) 0, band, training, true));
+    }
+
+    @Test
+    public void when_QueryingTrainingByBand_expect_RepositoryCalledPassback() {
         Mockito.when(bandTrainingRepository.findByBandNameOrderByTrainingType("")).thenReturn(bandTrainings);
-        BandTrainingService bandTrainingService = new BandTrainingService(bandTrainingRepository);
 
         List<Training> results = bandTrainingService.getTrainingByBandSortedByTrainingType("");
         Mockito.verify(bandTrainingRepository).findByBandNameOrderByTrainingType("");
+
+        assertEquals(trainings, results);
+    }
+
+    @Test
+    public void when_QueryingRecommendedTrainingByBand_expect_RepositoryCalledPassback() {
+        Mockito.when(bandTrainingRepository.findByBandNameAndRecommendedTrueOrderByTrainingType("")).thenReturn(bandTrainings);
+
+        List<Training> results = bandTrainingService.getRecommendedTrainingByBandSortedByTrainingType("");
+        Mockito.verify(bandTrainingRepository).findByBandNameAndRecommendedTrueOrderByTrainingType("");
+
+        assertEquals(trainings, results);
+    }
+
+    @Test
+    public void when_QueryingNonRecommendedTrainingByBand_expect_RepositoryCalledPassback() {
+        Mockito.when(bandTrainingRepository.findByBandNameAndRecommendedFalseOrderByTrainingType("")).thenReturn(bandTrainings);
+
+        List<Training> results = bandTrainingService.getNonRecommendedTrainingByBandSortedByTrainingType("");
+        Mockito.verify(bandTrainingRepository).findByBandNameAndRecommendedFalseOrderByTrainingType("");
 
         assertEquals(trainings, results);
     }
