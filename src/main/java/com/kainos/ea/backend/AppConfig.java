bc3715cc65ea7249e08879b58c157e659d8dfeae
michaltangri <michal.tangri@kainos.com>
2021-09-06 16:51:12 +0200

    024 Add API authorisation

---
+++ src/main/java/com/kainos/ea/backend/AppConfig.java
@@ -0,0 +1,31 @@
+package com.kainos.ea.backend;
+
+import com.kainos.ea.backend.interceptors.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan(basePackages = { "com.kainos.ea.*" })
+@PropertySource("classpath:application.properties")
+public class AppConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor(env)).addPathPatterns("/**");
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
